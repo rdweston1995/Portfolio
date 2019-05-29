904afd04c513ea1f83e7256988d38c1a358bee01
Robert <46203504+rdweston1995@users.noreply.github.com>
2019-05-28 23:45:24 -0700

    Mostly Done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,31 @@
-$(".AoG").on("click", function(){
-    window.open('https://rdweston1995.github.io/Project1/', '_blank');
-});
-
-$(".AoGcode").on('click', function(){
-    window.open("https://github.com/rdweston1995/Project1", '_blank');
+$(document).ready(function(){
+    $("#AoG").on("click", function(){
+        
+        window.open('https://rdweston1995.github.io/Project1/', '_blank');
+    });
+    
+    $(".AoGcode").on('click', function(){
+        
+        window.open("https://github.com/rdweston1995/Project1", '_blank');
+    })
+    
+    $(".MtnHng").on('click', function(){
+        window.open('https://rdweston1995.github.io/Word-Guess-Game/', '_blank');
+    });
+    
+    $(".Mtncode").on('click', function(){
+        window.open("https://github.com/rdweston1995/Word-Guess-Game", "_blank");
+    })
+    
+    $(".SpTr").on('click', function(){
+        window.open('https://rdweston1995.github.io/TriviaGame/', '_blank');
+    });
+    
+    $(".spaceCode").on('click', function(){
+        window.open("https://github.com/rdweston1995/TriviaGame", "_blank");
+    });
 })
 
-$(".MtnHng").on('click', function(){
-    window.open('https://rdweston1995.github.io/Word-Guess-Game/', '_blank');
-});
 
-$(".Mtncode").on('click', function(){
-    window.open("https://github.com/rdweston1995/Word-Guess-Game", "_blank");
-})
-
-$(".SpTr").on('click', function(){
-    window.open('https://rdweston1995.github.io/TriviaGame/', '_blank');
-});
-
-$(".spaceCode").on('click', function(){
-    window.open("https://github.com/rdweston1995/TriviaGame", "_blank");
-});
 
 
