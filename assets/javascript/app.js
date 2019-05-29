983d9a632b7f86221b06330621c22ece1b4832a3
Robert <46203504+rdweston1995@users.noreply.github.com>
2019-05-29 11:23:27 -0700

    Added Nav Bar and buttons for linking to site

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,48 @@
 $(document).ready(function(){
+    /*
     $("#AoG").on("click", function(){
         
         window.open('https://rdweston1995.github.io/Project1/', '_blank');
     });
+    */
+    //On Click functions for Area of Growth Site and code
+    $(".AoGsite").on('click', function(){
+        window.open("https://rdweston1995.github.io/Project1/", "_blank");
+    })
     
     $(".AoGcode").on('click', function(){
         
         window.open("https://github.com/rdweston1995/Project1", '_blank');
     })
     
+    /*
     $(".MtnHng").on('click', function(){
         window.open('https://rdweston1995.github.io/Word-Guess-Game/', '_blank');
     });
+    */
+    //On Clickfunctions for Mountain Hangman game site and code
+    $(".Mtnsite").on("click", function(){
+        window.open("https://rdweston1995.github.io/Word-Guess-Game/", "_blank");
+    })
     
     $(".Mtncode").on('click', function(){
         window.open("https://github.com/rdweston1995/Word-Guess-Game", "_blank");
     })
     
+    /*
     $(".SpTr").on('click', function(){
         window.open('https://rdweston1995.github.io/TriviaGame/', '_blank');
     });
+    */
     
+    //On Click functions for Space Tiriva Game site and code
     $(".spaceCode").on('click', function(){
         window.open("https://github.com/rdweston1995/TriviaGame", "_blank");
     });
+
+    $(".spaceSite").on('click', function(){
+        window.open("https://rdweston1995.github.io/TriviaGame/", "_blank");
+    })
 })
 
 
