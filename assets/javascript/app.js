bf99f7c275cd2ede08f2187afb94b3ce91d9fca6
Robert <46203504+rdweston1995@users.noreply.github.com>
2019-05-28 21:34:19 -0700

    Most of site done. Javascript file not working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,25 @@
+$(".AoG").on("click", function(){
+    window.open('https://rdweston1995.github.io/Project1/', '_blank');
+});
+
+$(".AoGcode").on('click', function(){
+    window.open("https://github.com/rdweston1995/Project1", '_blank');
+})
+
+$(".MtnHng").on('click', function(){
+    window.open('https://rdweston1995.github.io/Word-Guess-Game/', '_blank');
+});
+
+$(".Mtncode").on('click', function(){
+    window.open("https://github.com/rdweston1995/Word-Guess-Game", "_blank");
+})
+
+$(".SpTr").on('click', function(){
+    window.open('https://rdweston1995.github.io/TriviaGame/', '_blank');
+});
+
+$(".spaceCode").on('click', function(){
+    window.open("https://github.com/rdweston1995/TriviaGame", "_blank");
+});
 
 
