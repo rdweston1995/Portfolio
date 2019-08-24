2afdc1108d7867219ebf66874673701b2a6a6e8c
Robert <46203504+rdweston1995@users.noreply.github.com>
2019-08-24 10:32:00 -0700

    Added home page

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home/home";
 
 class app extends Component {
     render(){
         return (
-            <React.Fragment>
-                <p>Testing</p>
-            </React.Fragment>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route componet={Home} />
+                </Switch>
+            </Router>
         )
     }
 }
---
+++ src/pages/Home/home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./home.css";
+
+class home extends Component {
+    render(){
+        return (
+            <React.Fragment>
+                <p>Testing</p>
+            </React.Fragment>
+        )
+    }
+}
+
+export default home;
