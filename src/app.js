0a694f4446cc5accfba6def4f45cb8dcd0fc953d
Robert <46203504+rdweston1995@users.noreply.github.com>
2019-08-24 10:27:12 -0700

    Boiler plate files created and skeleton code added

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+class app extends Component {
+    render(){
+        return (
+            <React.Fragment>
+                <p>Testing</p>
+            </React.Fragment>
+        )
+    }
+}
+
+export default app;
