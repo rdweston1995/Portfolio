ae26c725ec559c487062d8f17821ee40db901c7e
Robert <46203504+rdweston1995@users.noreply.github.com>
2019-09-29 21:08:55 -0700

    Fixing CSS on home page and added the projects folder and related files

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home/home";
 import AboutMe from "./pages/aboutMe/aboutMe";
+import Projects from "./pages/Projects/projects";
 
 class app extends Component {
     render(){
@@ -11,6 +12,7 @@ class app extends Component {
                 <Switch>
                     <Route exact path="/" component={Home} />
                     <Route exact path="/aboutMe" component={AboutMe} />
+                    <Route exact path="/projcts" component={Projects} />
                     <Route component={Home} />
                 </Switch>
             </Router>
--- src/pages/aboutMe/aboutMe.js
+++ src/pages/aboutMe/aboutMe.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import "./aboutMe.css";
 
 class aboutMe extends Component{
     render(){
         return(
-            <div>
-                About Me
+            <div className="aboutMe">
+                <Row>
+                    <Col>
+                        <h2>About Me</h2>
+                        <p>About me About me</p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <h3>Skills</h3>
+                    </Col>
+                </Row>
             </div>
         )
     }
