64037e5815e57ac64cbbe88356e1b2981897a5f1
Robert <robert.d.weston@gmail.com>
2019-10-23 20:13:14 -0700

    Working on About me section

--- src/pages/aboutMe/aboutMe.js
+++ src/pages/aboutMe/aboutMe.js
@@ -16,13 +16,48 @@ class aboutMe extends Component{
             
             <div className="aboutMe">
                 {/* <div className="bufferZone"/> */}
-                <Row className="aboutMeRow">
+                {/* <Row className="aboutMeRow">
                     <Col className="aboutMeCol">
                         <h2 className="aboutMeTitle">About Me</h2>
                         <p className="aboutMeText">About me About me</p>
                     </Col>
                 </Row>
                 <Row className="skillsRow">
+                    <Col className="skillsCol">
+                        <h3 className="skillsTitle">Skills</h3>
+                        <h4 className="skillsSubTitle">Languages</h4>
+                        <ul className="languagesList">
+                            {this.state.languages.map(function(lang, i){
+                                return(
+                                    <ListItem skill={lang} key={i}/>
+                                )
+                            })}
+                        </ul>
+                        <h4 className="skillsSubTitle">Databases</h4>
+                        <ul className="databasesList">
+                            {this.state.databases.map(function(database, i){
+                                return(
+                                    <ListItem skill={database} key={i}/>
+                                )
+                            })}
+                        </ul>
+                        <h4 className="skillsSubTitle">Tools</h4>
+                        <ul className="toolsList">
+                            {this.state.tools.map(function(tool, i){
+                                return(
+                                    <ListItem skill={tool} key={i}/>
+                                )
+                            })}
+                        </ul>
+                    </Col>
+                </Row> */}
+
+                <Row className="aboutMeRow">
+                <h2 className="aboutMeTitle">About Me</h2>
+                    <Col className="aboutMeCol">
+                        <p className="aboutMeText">About me About me</p>
+                    </Col>
+
                     <Col className="skillsCol">
                         <h3 className="skillsTitle">Skills</h3>
                         <h4 className="skillsSubTitle">Languages</h4>
