877347ae571f12d81fef79b401ab5b177c46161d
Robert <robert.d.weston@gmail.com>
2019-12-16 20:43:34 -0800

    Working on loading projects and experience to the page using a json file

--- src/components/card.js
+++ src/components/card.js
@@ -1,22 +1,21 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
-function card(){
-    return(
-        <div>
-            <Card style={{width: "45%", height: "40vh", backgroundColor: "#262626", margin: "15px", float: "left"}}>
-                Test
-            </Card>
-            <Card style={{width: "45%", height: "40vh", backgroundColor: "#262626", margin: "15px", float: "left"}}>
-                Test
-            </Card>
-            <Card style={{width: "45%", height: "40vh", backgroundColor: "#262626", margin: "15px", float: "left"}}>
-                Test
-            </Card>
-            <Card style={{width: "45%", height: "40vh", backgroundColor: "#262626", margin: "15px", float: "left"}}>
-                Test
-            </Card>
-        </div>
+function card(props){
+    return(    
+        <Card className={props.className} style={{width: "48%", height: "750px", padding: "30px", margin: "15px", float: "left", backgroundColor : "#262626"}}>
+            <h2 classname="projectTitle" style={{fontSize : "50px", fontWeight: "bold", color : "#ffffff"}}>{props.title}</h2>
+            <img className="websiteImage"  src={props.image} style={{width: "100%", height: "500px"}} alt="project example"/>
+            <p className="cardText" style={{fontSize: "25px", color : "#ffffff"}}>
+                {props.text}
+            </p>
+            <div className="btnDiv" style={{width: "100%", textAlign: "left"}}>
+            <Button className="cardWebsiteButton" href={props.website} target="_blank" style={{display: "inline-block", width: "100px", fontSize: "20px", backgroundColor : "#5A5959", borderColor: "transparent"}}>Website</Button>
+            <Button className="cardGithubButton" href={props.github} target="_blank" style={{display: "inline-block", width: "100px", margin: "15px", fontSize: "20px", backgroundColor : "#5A5959", borderColor: "transparent"}}>Github</Button>
+            </div>
+            
+        </Card> 
     )
 }
 
--- src/pages/Projects/projects.js
+++ src/pages/Projects/projects.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "./../../components/card";
+import projectsJson from "./../../../src/projects.json";
 import "./projects.css";
 
 class Projects extends Component{
@@ -12,11 +13,26 @@ class Projects extends Component{
                 <Row className="projectsRow">
                     <h2 className="title">Projects</h2>
                     <Col className="projectsCol">
-                        <Card  className="testing"/>
+                        {projectsJson.map(function(project, i) {
+                            return (
+                                <Card 
+                                    className="projectCard btn-secondary"
+                                    title={project.title}
+                                    text={project.text}
+                                    website={project.website}
+                                    github={project.github}
+                                    image={project.image}
+                                    />
+                            )
+                        })}
+                        {/* <Card 
+                            className="testing1"
+                            text="Test Test"    
+                        />
+                        <Card className="testing2"/> */}
                     </Col>
                 </Row>
-            </div>
-            
+            </div>  
         )
     }
 }
