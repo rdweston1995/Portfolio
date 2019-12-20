bb0f53af7c537ffe530421c54e748bc8280cd6a8
Robert <robert.d.weston@gmail.com>
2019-12-19 17:33:23 -0800

    Added skills.json and have a filter function to find clicked skill and display the details

--- src/pages/aboutMe/aboutMe.js
+++ src/pages/aboutMe/aboutMe.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import ListItem from "./../../components/listItem";
+import skillsJSON from "./../../skills.json";
 import "./aboutMe.css";
 
 class aboutMe extends Component {
@@ -9,83 +10,28 @@ class aboutMe extends Component {
         languages: ['HTML', 'CSS', 'JavaScript', 'Java', 'Python', 'SQL'],
         databases: ['MySQL', 'MongoDB', 'DynamoDB'],
         tools: ['Git/Github', 'Node', 'REST API\'s', 'aJax', 'jQuery', 'React'],
-        displaySkill : false,
         displayedSkill : ""
     }
 
-    skillOnClick = (e, skill) => {
-        console.log(skill);
+    skillOnClick = (e, clickedSkill) => {
+        let foundSkill = skillsJSON.filter(skill => skill.skill === clickedSkill)
+        console.log(foundSkill[0].detail);
         this.setState(state => ({
-            displaySkill : true,
-            displayedSkill : skill
+            displayedSkill : foundSkill[0].detail
         }));
-        this.changeSkillDetail(skill);
-
-    }
-
-    changeSkillDetail = (skill) => {
-        if(this.state.displayedSkill === ""){
-            console.log("nothing displayed");
-        } else {
-            console.log(this.state.displayedSkill);
-        }
-        console.log("Testing out what skill returns: " + JSON.stringify(skill));
-        return (
-            <p>{JSON.stringify(skill)}</p>
-        )
     }
 
     render() {
         return (
 
             <div className="aboutMe">
-                {/* <div className="bufferZone"/> */}
-                {/* <Row className="aboutMeRow">
-                    <Col className="aboutMeCol">
-                        <h2 className="aboutMeTitle">About Me</h2>
-                        <p className="aboutMeText">About me About me</p>
-                    </Col>
-                </Row>
-                <Row className="skillsRow">
-                    <Col className="skillsCol">
-                        <h3 className="skillsTitle">Skills</h3>
-                        <h4 className="skillsSubTitle">Languages</h4>
-                        <ul className="languagesList">
-                            {this.state.languages.map(function(lang, i){
-                                return(
-                                    <ListItem skill={lang} key={i}/>
-                                )
-                            })}
-                        </ul>
-                        <h4 className="skillsSubTitle">Databases</h4>
-                        <ul className="databasesList">
-                            {this.state.databases.map(function(database, i){
-                                return(
-                                    <ListItem skill={database} key={i}/>
-                                )
-                            })}
-                        </ul>
-                        <h4 className="skillsSubTitle">Tools</h4>
-                        <ul className="toolsList">
-                            {this.state.tools.map(function(tool, i){
-                                return(
-                                    <ListItem skill={tool} key={i}/>
-                                )
-                            })}
-                        </ul>
-                    </Col>
-                </Row> */}
-
                 <Row className="aboutMeRow">
                     <h2 className="title">About Me</h2>
                     <Col className="aboutMeCol">
                         <div className="aboutMeSection">
                             <img className="profilePicture" src="https://i.imgur.com/CbfJJZL.jpg" alt="The man the myth the legend" />
                             <p className="aboutMeText" data-toggle="tooltip" data-placement="top" title="tooltip on top">About me About me</p>
-                            {/* <Button type="button" className="btn btn-lg btn-danger" data-toggle="popover" title="Popover title" data-content="testing">Testing</Button> */}
-
                         </div>
-
                     </Col>
 
                     <Col className="skillsCol">
@@ -118,8 +64,7 @@ class aboutMe extends Component {
                                 </ul>
                             </div>
                             <div className="skillsDetails" onChange={this.changeSkillDetail}>
-                                <p>
-                                    {/* {this.state.displaySkill ? 'Yes' : 'No'} */}
+                                <p className="skillDetailP">
                                     {this.state.displayedSkill}
                                 </p>
 
@@ -128,11 +73,6 @@ class aboutMe extends Component {
 
                     </Col>
                 </Row>
-                {/* <Button variant="success" onClick={<Modal />}>Click me to see</Button> */}
-                {/* <UseModal /> */}
-                {/* <OverlayTrigger trigger="click" placement="right" overlay={Popover}>
-                    <Button variant="success">Click me to see</Button>
-                </OverlayTrigger> */}
             </div>
         )
     }
