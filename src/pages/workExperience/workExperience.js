1b7c17e1b09a9b9c876f748ed0d7f2a54ef3de89
Robert <46203504+rdweston1995@users.noreply.github.com>
2020-04-20 23:03:22 -0700

    Init workExperience page

---
+++ src/pages/workExperience/workExperience.js
@@ -0,0 +1,15 @@
+//Node Imports
+import React from "react";
+
+//CSS
+import "./workExperience.css";
+
+class workExperience extends React.Component {
+    render() {
+        return (
+            <></>
+        )
+    }
+}
+
+export default workExperience;
