06836924bc140bf3d64694312c636920e8f58b84
Robert <robert.d.weston@gmail.com>
2020-04-24 20:10:44 -0700

    Moved projectCard css to seperate file

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -2,20 +2,20 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 
-function card(props){
-    return(    
-        <Card className={props.className} style={{width: "48%", height: "725px", padding: "30px", margin: "15px", float: "left", backgroundColor : "#262626", boxShadow : "2px 2px 5px #000000"}}>
-            <h2 className="projectTitle" style={{fontSize : "50px", fontWeight: "bold", color : "#ffffff"}}>{props.title}</h2>
-            <img className="websiteImage"  src={props.image} style={{width: "100%", height: "500px", boxShadow: "1px 2px 2px #000000"}} alt="project example"/>
-            <p className="cardText" style={{fontSize: "25px", color : "#ffffff"}}>
-                {props.text}
-            </p>
-            <div className="btnDiv" style={{width: "100%", textAlign: "left"}}>
-            <Button className="cardWebsiteButton" href={props.website} target="_blank" style={{display: "inline-block", width: "100px", fontSize: "20px", backgroundColor : "#5A5959", borderColor: "transparent"}}>Website</Button>
-            <Button className="cardGithubButton" href={props.github} target="_blank" style={{display: "inline-block", width: "100px", margin: "15px", fontSize: "20px", backgroundColor : "#5A5959", borderColor: "transparent"}}>Github</Button>
+//CSS
+import "./card.css";
+
+function card(props) {
+    return (
+        <Card id="projectCard">
+            <h2 className="projectTitle">{props.title}</h2>
+            <img className="websiteImage" src={props.image} alt="project example" />
+            <p className="projectText">{props.text}</p>
+            <div className="projectBtnDiv">
+                <Button id="projectWebsite" className="projectBtn" href={props.website} target="_blank">Website</Button>
+                <Button id="projectGithub" className="projectBtn" href={props.github} target="_blank">Github</Button>
             </div>
-            
-        </Card> 
+        </Card>
     )
 }
 
