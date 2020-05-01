5f6d7349c4c0b5188c7f9976a6ce24afaa5d0a58
Robert <46203504+rdweston1995@users.noreply.github.com>
2020-04-30 22:14:27 -0700

    Updated format of skills section

--- src/pages/aboutMe/aboutMe.js
+++ src/pages/aboutMe/aboutMe.js
@@ -17,14 +17,14 @@ class aboutMe extends Component {
         languages: ['HTML', 'CSS', 'JavaScript', 'Java', 'Python', 'SQL'],
         databases: ['MySQL', 'MongoDB', 'DynamoDB'],
         tools: ['Git/Github', 'Node', 'REST API\'s', 'aJax', 'jQuery', 'React'],
-        displayedSkill : ""
+        displayedSkill: ""
     }
 
     skillOnClick = (e, clickedSkill) => {
         let foundSkill = skillsJSON.filter(skill => skill.skill === clickedSkill)
 
         this.setState(state => ({
-            displayedSkill : foundSkill[0].detail
+            displayedSkill: foundSkill[0].detail
         }));
     }
 
@@ -33,53 +33,47 @@ class aboutMe extends Component {
 
             <div className="aboutMe">
                 <Row className="aboutMeRow">
-                    <SubHeading id="aboutMeHeading" heading="About Me"/>
-                    
-                        <div className="aboutMeDiv">
-                            <img className="profilePicture" src="https://i.imgur.com/CbfJJZL.jpg" alt="The man the myth the legend" />
-                            <p className="aboutMeText" id="aboutMeId" data-toggle="tooltip" data-placement="top" title="tooltip on top">{aboutMeJSON.text}</p>
-                        </div>
-                    
-                    
-                    <Col className="skillsCol">
-                        <div className="skillsDiv">
-                            <h3 className="skillsTitle" >Skills</h3>
-                            <div className="skillsList">
-                                <h4 className="skillsSubTitle">Languages</h4>
-                                <ul className="languagesList">
-                                    {this.state.languages.map((lang, i) => {
-                                        return (
-                                            <ListItem skill={lang} key={i} onClick={((e) => this.skillOnClick(e, lang))} />
-                                        )
-                                    })}
-                                </ul>
-                                <h4 className="skillsSubTitle">Databases</h4>
-                                <ul className="databasesList">
-                                    {this.state.databases.map((database, i) => {
-                                        return (
-                                            <ListItem skill={database} key={i} onClick={((e) => this.skillOnClick(e, database))} />
-                                        )
-                                    })}
-                                </ul>
-                                <h4 className="skillsSubTitle">Tools</h4>
-                                <ul className="toolsList">
-                                    {this.state.tools.map((tool, i) => {
-                                        return (
-                                            <ListItem skill={tool} key={i} onClick={((e) => this.skillOnClick(e, tool))} />
-                                        )
-                                    })}
-                                </ul>
-                            </div>
-                            <div className="skillsDetails" onChange={this.changeSkillDetail}>
-                                <p className="skillDetailP">
-                                    {this.state.displayedSkill}
-                                </p>
+                    <SubHeading id="aboutMeHeading" heading="About Me" />
 
-                            </div>
-                        </div>
-                    </Col>
-                    
+                    <div className="aboutMeDiv">
+                        <img className="profilePicture" src="https://i.imgur.com/CbfJJZL.jpg" alt="The man the myth the legend" />
+                        <p className="aboutMeText" id="aboutMeId" data-toggle="tooltip" data-placement="top" title="tooltip on top">{aboutMeJSON.text}</p>
+                    </div>
                 </Row>
+                
+                <Row className="skillsDiv">
+                        <h3 className="skillsTitle" >Skills</h3>
+                        <Col>
+                            <h4 className="skillsSubTitle">Languages</h4>
+                            <ul className="languagesList">
+                                {this.state.languages.map((lang, i) => {
+                                    return (
+                                        <ListItem skill={lang} key={i} onClick={((e) => this.skillOnClick(e, lang))} />
+                                    )
+                                })}
+                            </ul>
+                        </Col>
+                        <Col>
+                            <h4 className="skillsSubTitle">Databases</h4>
+                            <ul className="databasesList">
+                                {this.state.databases.map((database, i) => {
+                                    return (
+                                        <ListItem skill={database} key={i} onClick={((e) => this.skillOnClick(e, database))} />
+                                    )
+                                })}
+                            </ul>
+                        </Col>
+                        <Col>
+                            <h4 className="skillsSubTitle">Tools</h4>
+                            <ul className="toolsList">
+                                {this.state.tools.map((tool, i) => {
+                                    return (
+                                        <ListItem skill={tool} key={i} onClick={((e) => this.skillOnClick(e, tool))} />
+                                    )
+                                })}
+                            </ul>
+                        </Col>
+                    </Row>
             </div>
         )
     }
