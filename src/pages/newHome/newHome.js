2f715ee0969624c77de46f777b8ecdaf7b81985c
Robert <robert.d.weston@gmail.com>
2020-12-04 21:20:32 -0800

    init

---
+++ src/pages/newHome/newHome.js
@@ -0,0 +1,17 @@
+//Node
+import React, { Component } from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+//CSS
+import "./newHome.css";
+
+class newHome extends Component { 
+    render() {
+        return (
+            <div>Hello</div>
+        );
+    }
+}
+
+export default newHome;
