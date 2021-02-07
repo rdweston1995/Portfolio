f31a427d6b00f63dda4c76dd1e54aec3409a377c
Robert <robert.d.weston@gmail.com>
2021-02-07 01:21:42 -0800

    Stopped using bootstrap buttons, added picture for hikingApp

--- src/components/projectCard/projectCard.js
+++ src/components/projectCard/projectCard.js
@@ -9,12 +9,18 @@ import "./projectCard.css";
 function card(props) {
     return (
         <Card id="projectCard">
-            <h2 className="projectTitle text">{props.title}</h2>
+            <h2 className="projectTitle text font">{props.title}</h2>
             <img className="websiteImage" src={loadImages[props.image]} alt="project example" />
-            <p className="projectText text">{props.text}</p>
+            <p className="projectText text font">{props.text}</p>
             <div className="projectBtnDiv">
-                <Button id="projectWebsite" className="projectBtn text" href={props.website} target="_blank">Website</Button>
-                <Button id="projectGithub" className="projectBtn text" href={props.github} target="_blank">Github</Button>
+                {/* <Button id="projectWebsite" className="projectBtn text font" href={props.website} target="_blank">Website</Button> */}
+                <form action={props.website} target="_blank">
+                    <button id="projectWebsite" className="projectBtn text font" href={props.website} target="_blank">Website</button>
+                </form>
+                {/* <Button id="projectGithub" className="projectBtn text font" href={props.github} target="_blank">Github</Button> */}
+                <form action={props.github} target="_blank">
+                    <button id="projectWebsite" className="projectBtn text font" href={props.github} target="_blank">Github</button>
+                </form>
             </div>
         </Card>
     )
